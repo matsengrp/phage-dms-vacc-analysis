8922a6a1afc6e1e226aa4a63324023bbd35df551
jgallowa07 <jaredgalloway07@gmail.com>
2021-09-21 14:17:16 -0700

    added bio attr and metadata

--- SRA/organize_filenames.py
+++ SRA/organize_filenames.py
@@ -3,26 +3,46 @@ import pandas as pd
 from shutil import copyfile
 from collections import defaultdict
 import os
+import sys
 
-seq_dir_filenames = {"sample_name" : [], "filename0" : []}
+#sra_meta = {"sample_name" : [], "filename0" : []}
+sra_meta = defaultdict(list)
+bio_attr = defaultdict(list)
 unique = set()
 source_dupes = defaultdict(list)
-sample_table = pd.read_csv("sample_table.csv")
-sample_files = sample_table["seq_dir"] + sample_table["fastq_filename"]
+sample_table = pd.read_csv(sys.argv[1])
+#sample_files = sample_table["seq_dir"] + sample_table["fastq_filename"]
 #print(sample_files)
-for i, source in enumerate(sample_files):
-    dest = os.path.join("manuscript_fastq", os.path.basename(source))
-    if dest in unique:
+# for i, source in enumerate(sample_files):
+for i, (row, data) in enumerate(sample_table.iterrows()):
+    #source = data["seq_dir"] + data["fastq_filename"]
+    #dest = os.path.join("manuscript_fastq", os.path.basename(source))
+    #if dest in unique:
         #print(source, dest)
-        print("YOOOOOOOOOOOO")
-        break
-        dest = os.path.join("manuscript_fastq", "non-dupe-" + os.path.basename(source))
+        #print("YOOOOOOOOOOOO")
+        #break
+        #dest = os.path.join("manuscript_fastq", "non-dupe-" + os.path.basename(source))
     #source_dupes[os.path.basename(source)].append(source)
-    assert dest not in unique    
-    unique.add(dest)
-    seq_dir_filenames["sample_name"].append(f"sample_{i}")
-    seq_dir_filenames["filename0"].append(f"{dest}")
-    copyfile(source, dest)
+    #assert dest not in unique    
+    #unique.add(dest)
+    sra_meta["sample_name"].append(f"sample_{i}")
+    bio_attr["sample_name"].append(f"sample_{i}")
+    sra_meta["filename0"].append(os.path.basename(data["fastq_filename"]))
+    sra_meta["collection date"].append(data["experiment"])
+
+sra_meta["isolate"]="not applicable"
+sra_meta["host"]="Escherichia coli"
+sra_meta["lab host"]="Escherichia coli"
+sra_meta["geographic location"]="USA:Seattle"
+sra_meta["isolation source"] = "T7 phage library"
+
+bio_attr["Organism:"] = "Escherichia phage T7"
+bio_attr["Instrument:"] = "Illumina MiSeq"
+bio_attr["Strategy:"] = "CLONEEND"
+bio_attr["Source:"] = "GENOMIC"
+bio_attr["Selection:"] = "PCR"
+bio_attr["Layout:"] = "SINGLE"
+    #copyfile(source, dest)
 
 
 #for key, value in source_dupes.items():
@@ -32,9 +52,10 @@ for i, source in enumerate(sample_files):
 #    for f in filenames:
 #        assert f not in unique
 #        unique.add(f)
-#    seq_dir_filenames[seq_dir[:-14] + ".tar"] = pd.Series(filenames)
-seq_dir_df = pd.DataFrame(seq_dir_filenames)
+#    sra_meta[seq_dir[:-14] + ".tar"] = pd.Series(filenames)
+pd.DataFrame(sra_meta).set_index("sample_name").to_csv("sra_meta.tsv", "\t")
+pd.DataFrame(bio_attr).set_index("sample_name").to_csv("bio_attr.tsv", "\t")
 #column_name_map = {i : f"filename{i}" for i in range(seq_dir_df.shape[1])}
+
 #column_name_map["index"] = "sample_name"
 #seq_dir_df.rename(column_name_map, axis=1).to_csv("sra-metadata-filenames-example.tsv", "\t")
-seq_dir_df.to_csv("sra-metadata-filenames.tsv", "\t")
